a43af6f0d7019921890de390718028ff90c451c5
jonathanlaventhol <jonathanlaventhol@be63f5ac-731f-11de-8369-6b41a2a8383f>
2009-07-21 20:46:45 +0000

    Adding into repository

---
+++ amphibiousarchitecture/boh/htdocs/test2.php
@@ -0,0 +1,59 @@
+<?php
+
+# test2.php
+# simple web/php tests
+# jcl/nyu/2009-07-19
+
+# Subversion $Id$
+
+function dumptable($table) {
+	printf("<h2>%s</h2>", $table);
+
+	$sql = sprintf("select * from %s", $table);
+
+	$res = mysql_query($sql);
+	if (!$res) {
+		printf("problem with sql query");
+		return;
+	}
+
+	printf("<table border=1>");
+	$rownum = 0;
+	while ($row = mysql_fetch_assoc($res)) {
+		++$rownum;
+		if ($rownum == 1) {
+			printf("<tr>");
+			foreach ($row as $key => $col) {
+				printf("<th>%s\n", $key);
+			}
+		}
+		printf("<tr>");
+		foreach ($row as $key => $col) {
+			printf("<td>%s\n", $col);
+		}
+	}
+	printf("</table>");
+}
+
+function main($dbhost, $dbuser, $dbpass, $dbname) {
+	printf("<h1>Table Dump</h1>");
+	printf("(%s)\n", strftime("%Y%m%d-%H%M%S"));
+
+	$db = mysql_connect($dbhost, $dbuser, $dbpass);
+	if (!$db) {
+		die("can't connect to db $dbhost, $dbuser");
+	}
+	if (!mysql_select_db($dbname)) {
+		die("can't select db $dbname");
+	}
+
+	foreach (array("f1", "f2", "f3", "f4", "f5", "f6") as $table) {
+		dumptable($table);
+	}
+	printf("<hr>\n");
+}
+
+require_once("dbconfig.php");
+main($dbhost, $dbuser, $dbpass, $dbname);
+
+?>
