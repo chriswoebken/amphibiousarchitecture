9f9fc09fdf600229e257cedf1f72374e928e7e70
jonathanlaventhol <jonathanlaventhol@be63f5ac-731f-11de-8369-6b41a2a8383f>
2009-07-21 20:17:07 +0000

    Adding UDP server code to repository

---
+++ amphibiousarchitecture/boh/udpserver/tick.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# tick.sh
+# fake some data
+# jcl/nyu/2009-07-20
+
+site=99
+./udpsend 127.0.0.1 6000 "action=f1&site=$site&sensor=1&event=sms"
+
+# fake day-of-month as nfish, 100+minute as weight, 200+minute as depth
+#
+nfish="`date '+%d'`"
+weight="1`date '+%M'`"
+depth="2`date '+%M'`"
+./udpsend 127.0.0.1 6000 "action=f2&site=$site&sensor=1&nfish=$nfish&weight=$weight&depth=$depth"
+
+# fake 9000+seconds as dissox micrograms/liter
+#
+dissox="90`date '+%M'`"
+./udpsend 127.0.0.1 6000 "action=f3&site=$site&sensor=1&dissox=$dissox"
+
+foodtype=1
+weight=10
+./udpsend 127.0.0.1 6000 "action=f4&site=$site&sensor=1&foodtype=$foodtype&weight=$weight"
+
+caller=2125551212
+msg=eriver
+reply="river all good"
+./udpsend 127.0.0.1 6000 "action=f5&site=$site&sensor=1&caller=$caller&msg=$msg&reply=$reply"
+
+# end
